d48ea391d36811302cd487f35866176a631dd4c5
Tim Hüttemeister <tim.czubayko@googlemail.com>
2016-04-04 16:35:46 +0400

    Initial commit
    Initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# timmes.Boxstarter #
+
+### Summary ###
+Automating tasks is cool. It saves time and minimizes errors, at least manual errors. To save us (.NET developers) some time, I created this small script (inspired by @robertbird) for [Boxstarter](http://boxstarter.org/), so we can focus on more important things than manually downloading and installing our needed tools.
+
+### Prerequisites ###
+- Windows 10
+- Boxstarter
+
+In order to get started with the script, you need to install Boxstarter first. This is pretty easy. Simply run the following script from your Windows PowerShell:
+
+```
+choco install boxstarter -y
+```
+**You're wondering why this is working?**
+
+Windows 10 already has a *Package Manager* Manager, called [One-Get](https://github.com/OneGet/oneget), in place. With it also comes [Chocolatey](https://chocolatey.org): *"Chocolatey NuGet is a Machine Package Manager, somewhat like apt-get, but built with Windows in mind." (Quote)*
+
+### Running the script ###
+This is as easy as installing the prerequisites:
+1. Open Internet Explorer *(yes, I know... not my favorite choice either but necessary)*
+2. Copy & paste the following line and navigate to it
+```
+START http://boxstarter.org/package/nr/url?https://raw.githubusercontent.com/timmes/timmes.Boxstarter/master/timmes.Boxstarter.BasicDevMachineSetup.ps1
+```
+3. Grab a coffee (or tea) and relax! ;-)
+
+If you do not want to run the script *as is* but make your own changes, you should clone the repository or create a [Gist](https://gist.github.com/). Afterwards replace everything after the `url?` with the new URL where your script is hosted.
+
+----------
+
+# Disclaimer #
+**THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF ANY KIND.**
+
+----------
+
+# Contributing #
+If you want to contribute to this project, simply clone the repository and send a pull request with your changes.
+
+----------
+
+# License #
+See the [LICENSE](LICENSE.md) file for license rights and limitations (MIT).
---
+++ timmes.Boxstarter.BasicDevMachineSetup.ps1
@@ -0,0 +1,82 @@
+### GENERAL BOXSTARTER SETTINGS ###
+$Boxstarter.RebootOk=$true # Allow reboots?
+$Boxstarter.NoPassword=$false # Is this a machine with no login password?
+$Boxstarter.AutoLogin=$true # Save my password securely and auto-login after a reboot
+
+
+### WINDOWS SETTINGS ###
+# .NET 3.5
+choco install dotnet3.5
+if (Test-PendingReboot) { Invoke-Reboot }
+
+# .NET 4.5
+choco install dotnet4.5
+if (Test-PendingReboot) { Invoke-Reboot }
+
+# Basic setup
+Update-ExecutionPolicy Unrestricted
+Set-ExplorerOptions -showHidenFilesFoldersDrives -showFileExtensions
+Set-TaskbarSmall
+Enable-RemoteDesktop
+Disable-InternetExplorerESC
+Disable-UAC
+Move-LibraryDirectory "Personal" "$env:UserProfile\OneDrive\documents" #move "Documents" folder to OneDrive
+
+# Disable defrag (no need when having an SSD)
+Get-ScheduledTask -TaskName *defrag* | Disable-ScheduledTask
+
+
+### POWER SETTINGS ###
+# Turn off hibernation
+powercfg /H OFF
+
+# Change Power saving options (ac=plugged in dc=battery)
+powercfg -change -monitor-timeout-ac 0
+powercfg -change -monitor-timeout-dc 15
+powercfg -change -standby-timeout-ac 0
+powercfg -change -standby-timeout-dc 30
+powercfg -change -disk-timeout-ac 0
+powercfg -change -disk-timeout-dc 30
+powercfg -change -hibernate-timeout-ac 0
+
+
+### SOFTWARE ###
+# Browser #
+choco install googlechrome
+
+# Essentials & Productivity
+choco install office365business
+choco install skype
+choco install skypeforbusiness
+choco install dropbox
+choco install 1password #password manager
+choco install balsamiqmockups3
+choco install greenshot #best screenshot tool in the world
+choco install adobereader
+choco install windirstat #disk cleanup
+choco install 7zip
+cinst TelnetClient -source windowsFeatures
+
+# Development
+choco install visualstudio2015community
+choco install visualstudiocode
+choco install notepadplusplus
+choco install fiddler4
+choco install git
+choco install sourcetree #great gui for git
+
+# Databases
+choco install mssqlserver2014express
+
+# SharePoint
+#choco install sharepointmanager2013
+#choco install ulsviewer
+
+Install-ChocolateyPinnedTaskBarItem "$($Boxstarter.programFiles86)\Google\Chrome\Application\chrome.exe"
+Install-ChocolateyPinnedTaskBarItem "$($Boxstarter.programFiles86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.exe"
+Install-ChocolateyPinnedTaskBarItem "$($Boxstarter.programFiles86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.exe"
+Install-ChocolateyPinnedTaskBarItem "$($Boxstarter.programFiles86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.exe"
+
+# Update Windows and reboot if necessary
+Install-WindowsUpdate -AcceptEula -GetUpdatesFromMS
+if (Test-PendingReboot) { Invoke-Reboot }
